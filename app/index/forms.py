e9de388e29f78ac925ab617400c8f0a23cd4643a
kurapikaaaa <petersheng2@outlook.com>
2021-06-25 19:43:54 +0800

    c

--- app/index/forms.py
+++ app/index/forms.py
@@ -1,17 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField, StringField
+from wtforms import IntegerField, SubmitField, StringField, BooleanField
 from wtforms.validators import DataRequired 
 
 # register form
 class assetForm(FlaskForm):
+    name = StringField('Asset Name', validators=[DataRequired()])
     endofservice = IntegerField('endofservice', validators=[DataRequired()])
     budget = IntegerField('budget', validators=[DataRequired()])
     Carbon_p_y = IntegerField('Carbon_p_y', validators=[DataRequired()])
-    submit = SubmitField('submit')
+    submit = SubmitField('NEXT')
 
 class componentForm(FlaskForm):
     asset = StringField("parrent asset")
     decomCost_p_unit = IntegerField('User Name', validators=[DataRequired()])
-    decomUnit = IntegerField('First Name', validators=[DataRequired()])
-    CanBeUsedForHydrogenProduction = IntegerField('Last name', validators=[DataRequired()])
-    submit = SubmitField('submit')
+    decomUnit = IntegerField('', validators=[DataRequired()])
+    CanBeUsedForHydrogenProduction = BooleanField('Reuseable', validators=[DataRequired()])
+    moreComponents = BooleanField("More assets", default=True)
+    submit = SubmitField('finish')
--- app/index/routes.py
+++ app/index/routes.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, url_for, redirect, render_template, flash, jsonify
 from app.index import bp
-from app.index.forms import assetForm
+from app.index.forms import assetForm, componentForm
 from app import db
 from app.model import asset, component
 from app.model import asset
+from app.controller import add, getAssetByName
 import json
 
 #route to home page
@@ -13,9 +14,33 @@ def index():
     form = assetForm()
     if form.validate_on_submit():
         asse = asset()
-        asse.eof = form.endofservice.data
-        asse.carbon = form.Carbon_p_y.data
-        return render_template('success.html', form=form)
+        asse.name = form.name.data
+        asse.endofservice = form.endofservice.data
+        asse.budget = form.budget.data
+        asse.Carbon_p_y = form.Carbon_p_y.data
+        db.session.add(asse)
+        db.session.commit()
+        x = add()
+        return redirect(url_for('index.comp'))
+    return render_template('index.html', form=form)
+
+@bp.route('/comp',methods=['GET', 'POST'])
+def comp():
+    form = componentForm()
+    if form.validate_on_submit():
+        comp = component()
+        comp.decomCost_p_unit = form.decomCost_p_unit.data
+        comp.decomUnit = form.decomUnit.data
+        comp.CanBeUsedForHydrogenProduction = form.CanBeUsedForHydrogenProduction.data
+        a = getAssetByName(form.asset.data)
+        comp.asset_id = a
+        db.session.add(comp)
+        db.session.commit()
+        if form.moreComponents:
+            return redirect(url_for('index.comp'))
+        else:
+            x = add()
+            return render_template('success.html', form=form, add=a)
     return render_template('index.html', form=form)
 
     
